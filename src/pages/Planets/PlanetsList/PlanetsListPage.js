7fa53fa479b5627a67ce25182145793a284b2f0f
kilisoria <cristiansoria@gmail.com>
2021-04-05 15:18:10 -0300

    clean code

--- src/pages/Planets/PlanetsList/PlanetsListPage.js
+++ src/pages/Planets/PlanetsList/PlanetsListPage.js
@@ -159,6 +159,7 @@ const PlanetsListPage = ({ planet, getPlanets }) => {
                 .map((row, index) => {
                   const isItemSelected = isSelected(row.name);
                   const labelId = `enhanced-table-checkbox-${index}`;
+                  
                   return (
                     <StyledTableRow
                       hover
